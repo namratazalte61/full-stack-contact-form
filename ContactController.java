1467ca5f04c0792b57d01bfe905c800db877d543
namratazalte61 <namratazalte61@gmail.com>
2026-05-01 20:13:17 +0530

    Add files via upload

---
+++ ContactController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class ContactController {
+
+    @PostMapping("/contact")
+    public String handleForm(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String message) {
+
+        System.out.println("===== FORM DATA RECEIVED =====");
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Message: " + message);
+
+        return "Form submitted successfully!";
+    }
+}
